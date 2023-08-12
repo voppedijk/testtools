5514953c5405fbe719f6f23113561b58d4285e1a
voppedijk <vincentoppedijk@gmail.com>
2023-08-12 19:19:57 +0200

    Refactor IbanGenerator class.

--- src/utility/IbanGenerator.java
+++ src/utility/IbanGenerator.java
@@ -5,42 +5,46 @@ import java.util.Random;
 
 public class IbanGenerator {
 
+    private final String randomBankNumber;
+    private final String controlNumber;
+
+    private IbanGenerator() {
+        this.randomBankNumber = generateRandomNumber();
+        this.controlNumber = calculateControlNumber();
+    }
+
     public static String generateIban() {
         IbanGenerator ibanGenerator = new IbanGenerator();
 
-        return ibanGenerator.calculateControlNumber();
+        return "NL" + ibanGenerator.controlNumber + "INGB" + ibanGenerator.randomBankNumber;
     }
 
     private String calculateControlNumber() {
         String bankToNumber = "18231611";
-        String bank = generateRandomNumber();
         String country = "2321";
-        String controlNumber;
         String result;
 
-        String mergedNumber = bankToNumber + bank + country;
+        String mergedNumber = bankToNumber + randomBankNumber + country;
         BigInteger numberToCalculate = new BigInteger(mergedNumber);
         numberToCalculate = numberToCalculate.mod(BigInteger.valueOf(97));
         int parsedNumberToCalculate = numberToCalculate.intValue();
         parsedNumberToCalculate = 98 - parsedNumberToCalculate;
-        controlNumber = String.valueOf(parsedNumberToCalculate);
+        result = String.valueOf(parsedNumberToCalculate);
         if (parsedNumberToCalculate < 10) {
-            controlNumber = "0" + controlNumber;
+            result = "0" + result;
         }
-
-        result = "NL" + controlNumber + "INGB" + bank;
 
         return result;
     }
 
     private String generateRandomNumber() {
         Random rand = new Random();
-        String randomNumber = String.valueOf(rand.nextInt(Integer.MAX_VALUE));
+        StringBuilder randomNumber = new StringBuilder(String.valueOf(rand.nextInt(Integer.MAX_VALUE)));
 
         while (randomNumber.length() < 10) {
-            randomNumber = "0" + randomNumber;
+            randomNumber.insert(0, "0");
         }
 
-        return randomNumber;
+        return randomNumber.toString();
     }
 }
