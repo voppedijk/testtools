df0bc7fcea2878361c080a79131097d1b6cf77d6
voppedijk <vincentoppedijk@gmail.com>
2023-08-13 09:53:34 +0200

    Fix BsnGenerator.

--- src/utility/BsnGenerator.java
+++ src/utility/BsnGenerator.java
@@ -28,11 +28,11 @@ public class BsnGenerator {
         String str = String.valueOf(bsn);
 
         for (int i = 0; i < str.length() - 1; i++) {
-            sum = sum + ((long) str.charAt(i) * elevenTestNrMultiplier);
+            sum += (long) Character.getNumericValue(str.charAt(i)) * elevenTestNrMultiplier;
             elevenTestNrMultiplier = elevenTestNrMultiplier - 1;
         }
 
-        sum += str.charAt(str.length() - 1);
+        sum += Character.getNumericValue(str.charAt(str.length() - 1)) * -1;
         return sum % 11 == 0;
     }
 }
